370b090f1ec16e6f620d5ab536414166892c347c
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-02-20 20:13:06 +0500

    Made the Users Cards for the Admin Dashboard

--- frontend/app/(dashboard)/admin/page.tsx
+++ frontend/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,19 @@
+import { UserCards } from '@/src/components/UserCards'
 import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>Admin Page</div>
+    <div className='flex flex-col md:flex-row gap-4 p-4'>
+      <div className='w-full lg:w-2/3'>
+        <div className='flex gap-2 lg:gap-4 justify-between flex-wrap'>
+          <UserCards type="student" />
+          <UserCards type="teacher" />
+          <UserCards type="parent" />
+          <UserCards type="staff" />
+        </div>
+      </div>
+      <div className='bg-green-300 w-full lg:w-1/3'>right</div>
+    </div>
   )
 }
 
