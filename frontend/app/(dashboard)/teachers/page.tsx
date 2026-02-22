a34b33b94189323922ef90e0c30da2c755a57421
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-02-22 15:46:53 +0500

    Made the parents and Teachers Dashboard

--- frontend/app/(dashboard)/teachers/page.tsx
+++ frontend/app/(dashboard)/teachers/page.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Announcements from "@/src/components/Announcements";
+import BigCalendar from "@/src/components/BigCalendar";
+import EventCalander from "@/src/components/EventCalander";
 
 const TeachersPage = () => {
   return (
-    <div>Teachers Page</div>
+     <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      <div className="w-full xl:w-2/3 " >
+        <div className="h-full bg-white rounded-2xl p-4">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+           <Announcements />
+      </div>
+    </div>
   )
 }
 
