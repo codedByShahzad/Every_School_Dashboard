35f8ea1661f510277c19e47b2516aaf668cc3502
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-02-25 20:00:12 +0500

    make the Student Detail Page

--- frontend/app/(dashboard)/list/student/[id]/page.tsx
+++ frontend/app/(dashboard)/list/student/[id]/page.tsx
@@ -1,22 +1,21 @@
 import Announcements from "@/src/components/Announcements";
 import BigCalendar from "@/src/components/BigCalendar";
+import PerformanceChart from "@/src/components/PerformanceChart";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import PerformanceChart from '../../../../../src/components/PerformanceChart';
 
-const page = () => {
+const SingleStudentPage = () => {
   return (
-    <div className="flex-1 p-4 flex flex-col xl:flex-row gap-4">
-      {/* Left */}
-      <div className="w-full xl:w-2/3 ">
-        {/* Top */}
+    <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        {/* TOP */}
         <div className="flex flex-col lg:flex-row gap-4">
           {/* USER INFO CARD */}
           <div className="bg-sky py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
               <Image
-                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200"
                 alt=""
                 width={144}
                 height={144}
@@ -24,20 +23,13 @@ const page = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
-              <div className="flex items-center gap-4">
-                <h1 className="text-xl font-semibold">Leonard Snyder</h1>
-              </div>
+              <h1 className="text-xl font-semibold">Cameron Moran</h1>
               <p className="text-sm text-gray-500">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               </p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                  <Image
-                    src="/images/blood.png"
-                    alt=""
-                    width={14}
-                    height={14}
-                  />
+                  <Image src="/images/blood.png" alt="" width={14} height={14} />
                   <span>A+</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
@@ -49,20 +41,16 @@ const page = () => {
                   <span>user@gmail.com</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                  <Image
-                    src="/images/phone.png"
-                    alt=""
-                    width={14}
-                    height={14}
-                  />
+                  <Image src="/images/phone.png" alt="" width={14} height={14} />
                   <span>+1 234 567</span>
                 </div>
               </div>
             </div>
           </div>
-          {/* Small Card */}
+          {/* SMALL CARDS */}
           <div className="flex-1 flex gap-4 justify-between flex-wrap">
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[47%] 2xl:w-[48%]">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/images/singleAttendance.png"
                 alt=""
@@ -70,12 +58,13 @@ const page = () => {
                 height={24}
                 className="w-6 h-6"
               />
-              <div>
-                <h1 className="text-xl font-semibold">98%</h1>
+              <div className="">
+                <h1 className="text-xl font-semibold">90%</h1>
                 <span className="text-sm text-gray-400">Attendance</span>
               </div>
             </div>
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[47%] 2xl:w-[48%]">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/images/singleBranch.png"
                 alt=""
@@ -83,25 +72,27 @@ const page = () => {
                 height={24}
                 className="w-6 h-6"
               />
-              <div>
-                <h1 className="text-xl font-semibold">2</h1>
-                <span className="text-sm text-gray-400">Branches</span>
+              <div className="">
+                <h1 className="text-xl font-semibold">6th</h1>
+                <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[47%] 2xl:w-[48%]">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/images/singleLesson.png"
                 alt=""
                 width={24}
                 height={24}
                 className="w-6 h-6"
-              />w-
-              <div>
-                <h1 className="text-xl font-semibold">6</h1>
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">18</h1>
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
-            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[47%] 2xl:w-[48%]">
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/images/singleClass.png"
                 alt=""
@@ -109,38 +100,38 @@ const page = () => {
                 height={24}
                 className="w-6 h-6"
               />
-              <div>
-                <h1 className="text-xl font-semibold">6</h1>
-                <span className="text-sm text-gray-400">Classes</span>
+              <div className="">
+                <h1 className="text-xl font-semibold">6A</h1>
+                <span className="text-sm text-gray-400">Class</span>
               </div>
             </div>
           </div>
         </div>
-        {/* Bottom */}
+        {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-auto">
-            <h1>Teacher's Schedule</h1>
-            <BigCalendar />
+          <h1>Student&apos;s Schedule</h1>
+          <BigCalendar />
         </div>
       </div>
-      {/* Right */}
-      <div className="w-full xl:w-1/3 flex flex-col gap-4 ">
-      <div className="bg-white p-4 rounded-md">
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
             <Link className="p-3 rounded-md bg-skyLight" href="/">
-              Teacher&apos;s Classes
+              Student&apos;s Lessons
             </Link>
             <Link className="p-3 rounded-md bg-purpleLight" href="/">
-              Teacher&apos;s Students
-            </Link>
-            <Link className="p-3 rounded-md bg-yellowLight" href="/">
-              Teacher&apos;s Lessons
+              Student&apos;s Teachers
             </Link>
             <Link className="p-3 rounded-md bg-pink-50" href="/">
-              Teacher&apos;s Exams
+              Student&apos;s Exams
             </Link>
             <Link className="p-3 rounded-md bg-skyLight" href="/">
-              Teacher&apos;s Assignments
+              Student&apos;s Assignments
+            </Link>
+            <Link className="p-3 rounded-md bg-yellowLight" href="/">
+              Student&apos;s Results
             </Link>
           </div>
         </div>
@@ -151,4 +142,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default SingleStudentPage;
--- frontend/app/(dashboard)/list/student/page.tsx
+++ frontend/app/(dashboard)/list/student/page.tsx
@@ -76,7 +76,7 @@ const columns = [
 
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`} >
+          <Link href={`/list/student/${item.id}`} >
             <button className="w-7 h-7 flex items-center justify-center rounded-full cursor-pointer bg-sky">
               <Image src="/images/view.png" alt="" width={16} height={16}  />
             </button>
