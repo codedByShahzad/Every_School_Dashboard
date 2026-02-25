4adea05828b802dd0129abf3ebfa38f018d2060b
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-02-25 15:29:57 +0500

    All List Pages are Completed

--- frontend/app/(dashboard)/list/subjects/page.tsx
+++ frontend/app/(dashboard)/list/subjects/page.tsx
@@ -44,12 +44,12 @@ const columns = [
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`} >
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
+            <button className="w-7 h-7 flex items-center justify-center rounded-full cursor-pointer bg-sky">
               <Image src="/images/edit.png" alt="" width={16} height={16}  />
             </button>
           </Link>
           {role === "admin" && (
-             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple">
+             <button className="w-7 h-7 flex items-center justify-center rounded-full cursor-pointer bg-purple">
               <Image src="/images/delete.png" alt="" width={16} height={16}  />
             </button>
           )}
